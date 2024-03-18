b41ae3b6db477d724693e0f58db25821a3e001e9
Pranesh Deshmukh <praneshd2003@gmail.com>
2024-03-19 00:03:12 +0530

    Update index.js

--- sum-and-compound-Interest server/index.js
+++ sum-and-compound-Interest server/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.get("/sum", (req,res) => {
     const a=parseInt(req.query.a);
     const b=parseInt(req.query.b);
     const sum = a+b;
     res.send(sum.toString());
 });
-app.listen(8080);
+app.get("/interest", (req,res)=> {
+    const p= parseFloat(req.query.p);
+    const r= parseFloat(req.query.r);
+    const n= parseFloat(req.query.n);
+    const result = p*r*n 
+    res.send(result.toString());
+})
+app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+});
